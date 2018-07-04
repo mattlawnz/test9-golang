74a451ff7385a1dc59f9326da6bf11eb89b2f475
mattlawnz <matt.law@thewarehouse.co.nz>
2018-07-05 11:06:37 +1200

    added awesome

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "AWesome Jenkins X golang http example!"
+	title := "Even more AWesome Jenkins X golang http example!"
 
 	from := ""
 	if r.URL != nil {
